6f5bb03892f08740a9d3e6ae6b17c89f70293178
Aditya <adityamurali003@gmail.com>
2025-01-28 12:11:25 +0530

    Wrap the Layout in Session Provider

---
+++ providers/rootProvider.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { ThemeProvider } from "./themeProvider"
+import { SessionProvider } from "./sessionProvider"
+import { Toaster } from "@/components/ui/toaster"
+
+export function RootProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <SessionProvider>
+                {children}
+                <Toaster />
+            </SessionProvider>
+        </ThemeProvider>
+    )
+}
